c52e9f3e06638773f42bc6fa05ff6c93425fae0f
Daniel Correia <dduarte96@hotmail.com>
2017-11-24 19:41:57 +0000

    updated process script

--- Proj2/process_tweet.py
+++ Proj2/process_tweet.py
@@ -4,20 +4,19 @@ result = ["id_str", "user_id_str", "created_at", "lang", "hashtags", "retweeted_
 
 def clean_tweets():
 	open("clean_tweets00.txt","w").close()	 #reset file
-	with open("tweets00.txt","r") as fin, open("clean_tweets00.txt","a") as fout:
-		fout.write("|".join(result)+"\n")
+	with open("full_data.00.out","r") as fin, open("clean_tweets00.txt","a") as fout:
+		fout.write(",".join(result)+"\n")
 		for line in fin:
-			outline = line.strip().replace('\"','')[4:-1].split(",")
-			outline = [item.strip() for item in outline]
-			fout.write("|".join(outline)+"\n")
+			outline = line.strip()[3:].split("|")
+			fout.write(",".join(outline)+"\n")
 
 
 def get_hashtags(min_count=10):
 	hashtags = Counter()
 	with open("clean_tweets00.txt","r") as fin:
 		for line in fin:
-			l1 = line.strip().split("|")
-			for tag in l1[5][1:-1].split(" "):
+			l1 = line.strip().split(",")
+			for tag in l1[4].split("#"):
 				hashtags[tag] += 1
 
 		for k in list(hashtags):
@@ -30,12 +29,7 @@ def filter_by_language(lang="en"):
 	open(lang+"_clean_tweets00.txt","w").close()	 #reset file
 	with open("clean_tweets00.txt","r") as fin, open(lang+"_clean_tweets00.txt","a") as fout:
 		for line in fin:
-			l1 = line.strip().split("|")
+			l1 = line.strip().split(",")
 			if l1[3] == lang:
-				fout.write("|".join(l1)+"\n")
+				fout.write(",".join(l1)+"\n")
 				
-
-with open("clean_tweets00.txt","r") as fin:
-	for line in fin:
-		l1 = line.strip().split("|")
-		print(l1)
