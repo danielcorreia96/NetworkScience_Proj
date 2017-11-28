68b6d3e447bd81086393e924a719c6b612d4337c
Daniel Correia <dduarte96@hotmail.com>
2017-11-28 05:12:54 +0000

    optimized network analysis using graph-tool

--- Proj2/network_analysis.py
+++ Proj2/network_analysis.py
@@ -3,27 +3,34 @@ import networkx as nx
 import pylab as plt
 from collections import Counter
 import argparse
+from graph_tool.all import *
 
 def extract_network(filename):
 	dataset = pd.read_csv(filename)
-	graph = dataset[["user_id_str","retweeted_user_id_str"]]
-	net = nx.from_pandas_dataframe(graph, "retweeted_user_id_str", "user_id_str", create_using=nx.MultiDiGraph())
+	graph = dataset[["retweeted_user_id_str","user_id_str"]]
+	# networkx version
+	# net = nx.from_pandas_dataframe(graph, "retweeted_user_id_str", "user_id_str", create_using=nx.MultiDiGraph())	
+	net = Graph()
+	vmap = net.add_edge_list(graph.values, hashed=True)
 	net.name = filename.split("/")[1][:-4]
-	print(nx.info(net))
+	# print(nx.info(net))
 	return net
 
 def do_degree_stuff(graph):
-	in_degrees = dict(graph.in_degree())
-	in_values = sorted(set(in_degrees.values()))
-	in_counts = Counter(in_degrees.values())
-	in_hist = [in_counts[x] for x in in_values]
+	# networkx version
+	# in_degrees = dict(graph.in_degree())
+	# in_values = sorted(set(in_degrees.values()))
+	# in_counts = Counter(in_degrees.values())
+	# in_hist = [in_counts[x] for x in in_values]
+	# out_degrees = dict(graph.out_degree())
+	# out_values = sorted(set(out_degrees.values()))
+	# out_counts = Counter(out_degrees.values())
+	# out_hist = [out_counts[x] for x in out_values]
 
-
-	out_degrees = dict(graph.out_degree())
-	out_values = sorted(set(out_degrees.values()))
-	out_counts = Counter(out_degrees.values())
-	out_hist = [out_counts[x] for x in out_values]
-
+	in_vals_hist = vertex_hist(graph,"in")
+	out_vals_hist = vertex_hist(graph,"out")
+	in_values, in_hist = in_vals_hist[1][:-1], in_vals_hist[0]
+	out_values, out_hist = out_vals_hist[1][:-1], out_vals_hist[0]
 
 	fig = plt.figure()
 	fig.set_figheight(7.5)
@@ -57,5 +64,5 @@ if __name__ == '__main__':
 	parser.add_argument("dataset",help="dataset to be processed")
 	args = parser.parse_args()
 
-	#graph = extract_network(args.dataset)
-	#do_degree_stuff(graph)
+	graph = extract_network(args.dataset)
+	do_degree_stuff(graph)
